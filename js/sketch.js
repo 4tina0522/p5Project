ed7ac8d75266b89f539181189508168370562d93
4ina0522 <rlu2@aticedu>
2021-02-03 21:41:24 -0600

    change

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,28 @@
+let ahyeah;
+let waving;
+let sitting;
+
+function preload(){
+  ahyeah = loadimage("images/ahyeah.png");
+  waving = loadimage("images/waving.gif");
+  sitting = loadimage("images/sitting.gif");
+}
 function setup() {
   // put setup code here to run once
   //create canvas 500 by 500 px
   createCanvas(windowWidth,windowHeight)
-  background(random(147), random(200), random(204))
+  background(random(255), random(255), random(255))
   //change
   rectMode(CENTER)
-
+  imageMode(CENTER)
   print(random(300))
 }
 
 
 function draw() {
+  background(180, 40, 130);
+  image(ahyeah, windowWidth/2, windowHeight/2)
+
   // put drawing code here to run in a loop
 
 
