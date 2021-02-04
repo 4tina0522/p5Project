d06bda8148e23d0a57c9697f9210c8f8ab67adef
4ina0522 <rlu2@aticedu>
2021-02-03 23:15:14 -0600

    1

--- js/sketch.js
+++ js/sketch.js
@@ -1,26 +1,21 @@
-let ahyeah;
-let waving;
-let sitting;
-
-function preload(){
-  ahyeah = loadimage('image/ahyeah.png');
-  waving = loadimage("image/waving.gif");
-  sitting = loadimage("image/sitting.gif");
-}
 function setup() {
   // put setup code here to run once
   //create canvas 500 by 500 px
   createCanvas(windowWidth,windowHeight)
   background(171,208,205)
-  image(ahyeah, 30, 30);
 }
 
 
 function draw() {
-  image(ahyeah, windowWidth/2, windowHeight/2)
   background(171,208,205)
-
+  smooth();
+  noStroke();
   // put drawing code here to run in a loop
 
+  //styles for the rect
+  stroke(200, 255, 80);
+  strokeWeight(50);
+  fill(255, 208, 205);
+  rect(400, 500, 300, 400);
 
 }
